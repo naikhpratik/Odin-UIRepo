c859821dbd76648118680d4163653c0966695d98
Austin Emser <austin.emser@dwellworks.com>
2017-08-23 14:50:14 -0400

    Init commit

--- MyDwellworks/App_Start/BundleConfig.cs
+++ MyDwellworks/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MyDwellworks
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-paper.css",
                       "~/Content/site.css"));
         }
     }
