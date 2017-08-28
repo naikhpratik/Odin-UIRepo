4624729f76bcf57737d56851810baaa5b1245f4b
Austin Emser <austin.emser@gmail.com>
2017-08-28 12:51:09 -0400

    Add test project

---
+++ MyDwellworks.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyDwellworks.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyDwellworks.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16df4b51-f65f-4fdf-8abc-6e5844bde941")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- MyDwellworks/Controllers/Api/AccountController.cs
+++ MyDwellworks/Controllers/Api/AccountController.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MyDwellworks.Data.Core;
 
 namespace MyDwellworks.Controllers.Api
 {
     [Authorize]
     public class AccountController : ApiController
     {
+        private readonly IUnitOfWork _unitOfWork;
 
+        public AccountController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IHttpActionResult GetUsers()
+        {
+
+
+            return Ok();
+        }
     }
 }
