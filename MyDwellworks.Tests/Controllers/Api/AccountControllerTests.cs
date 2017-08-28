952143854f8385df5e85620e7c494578717fe869
Austin Emser <austin.emser@gmail.com>
2017-08-28 15:48:56 -0400

    Add mock

---
+++ MyDwellworks.Tests/Controllers/Api/AccountControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDwellworks.Controllers.Api;
+
+namespace MyDwellworks.Tests.Controllers.Api
+{
+    [TestClass]
+    public class AccountControllerTests
+    {
+        private AccountController _controller; 
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+    }
+}
