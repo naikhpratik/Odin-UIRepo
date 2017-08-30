83ede83ebbfa9afc9824393cebdb6f09c54e2c14
Austin Emser <austin.emser@dwellworks.com>
2017-08-30 16:54:08 -0400

    Add the base objects

---
+++ Odin.Data/Core/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin.Data.Core.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string TrackingId { get; set; }
+        public string RelocationType { get; set; }
+        public string DestinationCity { get; set; }
+        public string DestinationState { get; set; }
+        public string DestinationZip { get; set; }
+        public string DestinationCountry { get; set; }
+        public string OriginCity { get; set; }
+        public string OriginState { get; set; }
+        public string OriginCountry { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public int TransfereeId { get; set; }
+        public Rent Rent { get; set; }
+    }
+}
