60b02072ce6d91d89894ee9cbb10be31160313e8
Austin Emser <austin.emser@dwellworks.com>
2017-09-05 15:52:25 -0400

    Add controller extension to unit test projecT

---
+++ Odin.Tests/Controllers/OrdersControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Odin.Controllers;
+
+namespace Odin.Tests.Controllers
+{
+    [TestClass]
+    public class OrdersControllerTests
+    {
+        private OrdersController _controller;
+        
+    }
+}
---
+++ Odin.Tests/Extensions/ControllerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+
+namespace Odin.Tests.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static void MockCurrentUser(this Controller controller, string userId)
+        {
+            var controllerContext = new Mock<ControllerContext>();
+            var claim = new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId);
+
+            var claimsIdentity = new ClaimsIdentity(new[] { claim });
+            var principal = new Moq.Mock<IPrincipal>();
+            principal.SetupGet(x => x.Identity).Returns(claimsIdentity);
+            controllerContext.SetupGet(x => x.HttpContext.User).Returns(principal.Object);
+
+            controller.ControllerContext = controllerContext.Object;
+        }
+    }
+}
