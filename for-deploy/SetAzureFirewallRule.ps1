1c26307ec1a4820a0a25eb97ca59a6e87ea77a4a
Austin Emser <austin.emser@dwellworks.com>
2017-09-07 11:15:01 -0400

    Change parameters

--- for-deploy/SetAzureFirewallRule.ps1
+++ for-deploy/SetAzureFirewallRule.ps1
@@ -1,24 +1,24 @@
-[CmdletBinding(DefaultParameterSetName = 'None')]
 param
 (
-  [String] [Parameter(Mandatory = $true)] $ServerName,
-  [String] $AzureFirewallName = "AzureWebAppFirewall"
+  [String]  
+  $AzureServerName,
+
+  [String] 
+  $AzureFirewallName = "AzureWebAppFirewall"
 )
 
 $ErrorActionPreference = 'Stop'
 
 function New-AzureSQLServerFirewallRule {
   $agentIP = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
-  New-AzureSqlDatabaseServerFirewallRule -StartIPAddress $agentIp -EndIPAddress $agentIp -RuleName $AzureFirewallName -ServerName $ServerName
+  New-AzureSqlDatabaseServerFirewallRule -StartIPAddress $agentIp -EndIPAddress $agentIp -RuleName $AzureFirewallName -ServerName $AzureServerName
 }
 function Update-AzureSQLServerFirewallRule{
   $agentIP= (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
-  Set-AzureSqlDatabaseServerFirewallRule -StartIPAddress $agentIp -EndIPAddress $agentIp -RuleName $AzureFirewallName -ServerName $ServerName
+  Set-AzureSqlDatabaseServerFirewallRule -StartIPAddress $agentIp -EndIPAddress $agentIp -RuleName $AzureFirewallName -ServerName $AzureServerName
 }
 
-Select-AzureSubscription -Default "Free Trial" 
-
-If ((Get-AzureSqlDatabaseServerFirewallRule -ServerName $ServerName -RuleName $AzureFirewallName -ErrorAction SilentlyContinue) -eq $null)
+If ((Get-AzureSqlDatabaseServerFirewallRule -ServerName $AzureServerName -RuleName $AzureFirewallName -ErrorAction SilentlyContinue) -eq $null)
 {
   New-AzureSQLServerFirewallRule
 }
