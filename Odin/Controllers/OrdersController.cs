36ec22c0786875a7e7d998e0062a037b882bc2c5
Austin Emser <austin.emser@dwellworks.com>
2017-10-02 14:24:57 -0400

    Clean up code and reimport nuget packages

--- Odin/Controllers/OrdersController.cs
+++ Odin/Controllers/OrdersController.cs
@@ -21,24 +21,19 @@ namespace Odin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        //Uncomment to test or use the SendEmailConfirmationTokenAsync method
-        //private IAccountHelper _accountHelper;
-        public OrdersController(IUnitOfWork unitOfWork, IMapper mapper) //,IAccountHelper accountHelper)
+        
+        public OrdersController(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
-            //Uncomment to test or use the SendEmailConfirmationTokenAsync method
-           // _accountHelper = accountHelper;
+
         }
 
     // GET: Orders
     public ViewResult Index()
         {
             var userId = User.Identity.GetUserId();
-            //Uncomment to test the SendEmailConfirmationTokenAsync method
-           // _accountHelper.SendEmailConfirmationTokenAsync("06bb5638-796e-4fb7-8e4e-dd95d898b123");
 
-            //SendEmailConfirmationTokenAsync("06bb5638-796e-4fb7-8e4e-dd95d898b123").Wait();
             var orders = _unitOfWork.Orders.GetOrdersFor(userId);
 
             var orderVms = _mapper.Map<IEnumerable<Order>, IEnumerable<OrderIndexViewModel>>(orders);
--- Odin/Helpers/AccountHelper.cs
+++ Odin/Helpers/AccountHelper.cs
@@ -1,11 +1,11 @@
 ﻿
 using Microsoft.AspNet.Identity.Owin;
 using Odin.Interfaces;
-using RazorEngine;
 using System;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using RazorEngine;
 
 namespace Odin.Helpers
 {
@@ -34,14 +34,14 @@ namespace Odin.Helpers
         public async Task<string> SendEmailConfirmationTokenAsync(string userID) //, string subject, string bdy)
         {
             var url = new UrlHelper(HttpContext.Current.Request.RequestContext);
-            //ApplicationUserManager userM = new ApplicationUserManager();
+            
             //Forgery check
             string code = await UserManager.GenerateEmailConfirmationTokenAsync(userID);
             //The link sent to the Transferees for access to reset password
             var callbackUrl = url.Action("ResetPassword", "Account", new { userID, code = code }, protocol: HttpContext.Current.Request.Url.Scheme);           
             //transferee's email address
             var eml = await UserManager.GetEmailAsync(userID);
-            //var user = UserManager.FindByName(eml);
+            
             var name = eml.Substring(0, eml.IndexOf("@")).Replace(".", " ");// user.FirstName + " " + user.LastName; 
             var subject = "Create Password";
             var templateFolderPath = HttpContext.Current.Server.MapPath(@"~\Views\Mailers\");
@@ -50,6 +50,6 @@ namespace Odin.Helpers
             //send the email, specify the content mime type
             var response = _emailHelper.SendEmail_SG(eml, subject, body, SendGrid.MimeType.Html);
             return callbackUrl;
-            }
+        }
     }
 }
