92c718f87be5b6a730bd2ab1e008f2f8f7f225b1
Austin Emser <austin.emser@dwellworks.com>
2017-10-05 10:56:26 -0400

    Add back old migration

---
+++ Odin.Data/Persistence/Migrations/201709201950074_PopulateSeContactUidInUsers.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+
+namespace Odin.Data.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateSeContactUidInUsers : DbMigration
+    {
+        public override void Up()
+        {
+            //var context = new ApplicationDbContext();
+            //var users = context.Users.ToList();
+
+            //for (int i = 0; i < users.Count; i++)
+            //    users[i].SeContactUid = i+1;
+
+            //context.SaveChanges();
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
