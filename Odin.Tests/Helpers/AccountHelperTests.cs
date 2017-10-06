6865ab9761b59b977660f2b72e7421ccdf393774
Matt Akl <makl@DWELLWORKS.com>
2017-10-06 14:39:09 -0400

    Added account helper test

---
+++ Odin.Tests/Helpers/AccountHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Odin.Interfaces;
+using Odin.Helpers;
+
+namespace Odin.Tests.Helpers
+{
+    [TestClass]
+    class AccountHelperTests
+    {
+            private IAccountHelper _accountHelper;
+                   
+            [TestMethod]
+            public void Index_WhenCalled_ReturnsOk()
+            {
+                var mockEmailHelper = new Mock<IEmailHelper>();  
+                _accountHelper = new AccountHelper(mockEmailHelper.Object);
+                string _userId = "1";
+                var result = _accountHelper.SendEmailCreateTokenAsync(_userId);
+                result.Should().NotBe("Message not sent");
+            }
+        }
+    }
