80b98c64cfab63e6ba5f207e05396fe8c1ef2648
Chris Nurre <CNurre@dwellworks.com>
2017-10-25 09:22:23 -0400

    WIP: sticky nav

--- Odin/Scripts/app/views/transferee.js
+++ Odin/Scripts/app/views/transferee.js
@@ -1,14 +1,27 @@
 ﻿var OrdersPageController = function () {
      
     var init = function () {
-        $('div.col > div').each(function() { $(this).css("display", "none"); });
-        $('div.col > div#intake').css("display", "block");
+            window.alert('hide all content');
+        $('div.col-md-10 > div > div').each(function () { $(this).css("display", "none"); });
+
+        window.alert('show intake content');
+        $('#intake').css("display", "block");
+
         $('.item').click(function () {
+            window.alert('remove selected from currently select item');
             $('.item').removeClass('selected');
+
+            window.alert('add selected class to the item');
+            
             $(this).addClass('selected');
 
-            $('div.col > div').each(function() { $(this).css("display", "none"); });
+            window.alert('Hide the other content');
+
+            $('div.col-md-10 > div').each(function() { $(this).css("display", "none"); });      
             $('div.col > div.frame').each(function () { $(this).css("display", "none"); });
+
+            window.alert('show the picked content');
+
             $('div.col > div#' + $(this).attr('data-panel')).css("display", "block");
         });
 
