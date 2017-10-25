355036c04db2121bbeace8b3fcfdf335f6302f0c
Holmes, Bill <BHolmes@dwellworks.com>
2017-10-25 11:38:23 -0400

    wip details needs polish

--- Odin/Scripts/app/views/transferee.js
+++ Odin/Scripts/app/views/transferee.js
@@ -1,28 +1,21 @@
 ﻿var OrdersPageController = function () {
      
     var init = function () {
-            window.alert('hide all content');
+
         $('div.col-md-10 > div > div').each(function () { $(this).css("display", "none"); });
 
-        window.alert('show intake content');
         $('#intake').css("display", "block");
 
         $('.item').click(function () {
-            window.alert('remove selected from currently select item');
             $('.item').removeClass('selected');
-
-            window.alert('add selected class to the item');
             
             $(this).addClass('selected');
 
-            window.alert('Hide the other content');
+            $('div.col-md-10 > div > div').each(function () { $(this).css("display", "none"); });
+            
+            //$('div.col > div.frame').each(function () { $(this).css("display", "none"); });
 
-            $('div.col-md-10 > div').each(function() { $(this).css("display", "none"); });      
-            $('div.col > div.frame').each(function () { $(this).css("display", "none"); });
-
-            window.alert('show the picked content');
-
-            $('div.col > div#' + $(this).attr('data-panel')).css("display", "block");
+            $('div#' + $(this).attr('data-panel')).css("display", "block");
         });
 
         // ensure icon spacing is adequate on mobile
