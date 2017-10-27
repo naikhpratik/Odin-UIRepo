562399ec79dd3de88ebfed22aa39534e911e014f
Austin Emser <austin.emser@dwellworks.com>
2017-10-27 13:44:39 -0400

    Include lease builder in porject

--- Odin.Data/Builders/LeaseBuilder.cs
+++ Odin.Data/Builders/LeaseBuilder.cs
@@ -8,8 +8,9 @@ namespace Odin.Data.Builders
         public static Lease New()
         {
             Lease lease = new Faker<Lease>()
-            .RuleFor(l => l.LeaseTerm, f => f.PickRandom(new int[]{1,2,3,4,5,6,7,8,9,10,11,12}))
-            .RuleFor(l => l.LengthOfAssignment, f => f.PickRandom(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }))
+                .RuleFor(l => l.LeaseTerm, f => f.PickRandom(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}))
+                .RuleFor(l => l.LengthOfAssignment,
+                    f => f.PickRandom(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}));
 
             return new Lease();
         }
