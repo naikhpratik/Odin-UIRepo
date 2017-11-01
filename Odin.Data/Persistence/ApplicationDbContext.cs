4284d3f54d96221e0f7a557345144546220c095b
Chris Nurre <CNurre@dwellworks.com>
2017-11-01 14:53:59 -0400

    Adding the join HomeFindingProperty object and table

--- Odin.Data/Persistence/ApplicationDbContext.cs
+++ Odin.Data/Persistence/ApplicationDbContext.cs
@@ -33,6 +33,7 @@ namespace Odin.Data.Persistence
         public virtual DbSet<BrokerFeeType> BrokerFeeTypes { get; set; }
 
         public DbSet<Property> Properties { get; set; }
+        public DbSet<HomeFindingProperty> HomeFindingProperties { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
---
+++ Odin.Data/Persistence/Migrations/201711011848429_AddHomeFindingPropertyJoin.cs
@@ -0,0 +1,116 @@
+namespace Odin.Data.Persistence.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddHomeFindingPropertyJoin : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.HomeFindingProperties",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                { 
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "Id")
+                                },
+                            }),
+                        Version = c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion",
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                { 
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "Version")
+                                },
+                            }),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                { 
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "CreatedAt")
+                                },
+                            }),
+                        UpdatedAt = c.DateTimeOffset(precision: 7,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                { 
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "UpdatedAt")
+                                },
+                            }),
+                        Deleted = c.Boolean(nullable: false,
+                            annotations: new Dictionary<string, AnnotationValues>
+                            {
+                                { 
+                                    "ServiceTableColumn",
+                                    new AnnotationValues(oldValue: null, newValue: "Deleted")
+                                },
+                            }),
+                        HomeFindingService_Id = c.String(maxLength: 128),
+                        Property_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Rents", t => t.HomeFindingService_Id)
+                .ForeignKey("dbo.Properties", t => t.Property_Id)
+                .Index(t => t.CreatedAt, clustered: true)
+                .Index(t => t.HomeFindingService_Id)
+                .Index(t => t.Property_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.HomeFindingProperties", "Property_Id", "dbo.Properties");
+            DropForeignKey("dbo.HomeFindingProperties", "HomeFindingService_Id", "dbo.Rents");
+            DropIndex("dbo.HomeFindingProperties", new[] { "Property_Id" });
+            DropIndex("dbo.HomeFindingProperties", new[] { "HomeFindingService_Id" });
+            DropIndex("dbo.HomeFindingProperties", new[] { "CreatedAt" });
+            DropTable("dbo.HomeFindingProperties",
+                removedColumnAnnotations: new Dictionary<string, IDictionary<string, object>>
+                {
+                    {
+                        "CreatedAt",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "CreatedAt" },
+                        }
+                    },
+                    {
+                        "Deleted",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "Deleted" },
+                        }
+                    },
+                    {
+                        "Id",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "Id" },
+                        }
+                    },
+                    {
+                        "UpdatedAt",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "UpdatedAt" },
+                        }
+                    },
+                    {
+                        "Version",
+                        new Dictionary<string, object>
+                        {
+                            { "ServiceTableColumn", "Version" },
+                        }
+                    },
+                });
+        }
+    }
+}
