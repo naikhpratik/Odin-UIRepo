accc82f1f3136496eec28979106dee8588382f90
Austin Emser <austin.emser@dwellworks.com>
2017-11-06 17:33:41 -0500

    Add untracked

---
+++ Odin.Data/ServiceClients/IImageStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Odin.Data.ServiceClients
+{
+    public interface IImageStore
+    {
+        Uri UriFor(string iamgeId);
+        Task<string> SaveImage(Stream stream);
+    }
+}
---
+++ Odin.Data/ServiceClients/ImageStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin.Data.ServiceClients
+{
+    public class ImageStore : IImageStore
+    {
+        private FakeBlobClient _client;
+        private Uri _cloudUri;
+
+        public ImageStore(string storageKey, string accountName, Uri cloudUri)
+        {
+            _cloudUri = cloudUri;
+            _client = new FakeBlobClient(cloudUri, accountName,storageKey);
+        }
+
+        public async Task<string> SaveImage(Stream stream)
+        {
+
+            return "Test";
+        }
+
+        public Uri UriFor(string iamgeId)
+        {
+            
+            return new Uri("test");    
+        }
+    }
+}
