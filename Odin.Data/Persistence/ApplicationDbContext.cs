0a8ed6fcc44e38ce6b33f9bec714f54e6e7e4401
Chris Nurre <CNurre@dwellworks.com>
2017-11-08 11:48:10 -0500

    Adding Cascade deletion for properties from homefindingproperties

--- Odin.Data/Persistence/ApplicationDbContext.cs
+++ Odin.Data/Persistence/ApplicationDbContext.cs
@@ -49,10 +49,7 @@ namespace Odin.Data.Persistence
         {
             modelBuilder.Configurations.Add(new OrderConfiguration());
             modelBuilder.Configurations.Add(new HomeFindingConfiguration());
-
-            //modelBuilder.Entity<Child>()
-            //    .Map(m => m.Requires("Deleted").HasValue(false))
-            //    .Ignore(m => m.Deleted);
+            modelBuilder.Configurations.Add(new HomeFindingPropertiesConfiguration());
 
             modelBuilder.Conventions.Add(
                 new AttributeToColumnAnnotationConvention<TableColumnAttribute, string>(
---
+++ Odin.Data/Persistence/EntityConfigurations/HomeFindingPropertiesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Odin.Data.Core.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Odin.Data.Persistence.EntityConfigurations
+{
+    public class HomeFindingPropertiesConfiguration : EntityTypeConfiguration<HomeFindingProperty>
+    {
+        public HomeFindingPropertiesConfiguration()
+        {
+            //HasRequired(hfp => hfp.HomeFinding)
+            //    .WithMany(hf => hf.HomeFindingProperties)
+            //    .WillCascadeOnDelete(true);
+            
+            HasRequired(hfp => hfp.Property)
+                .WithMany()
+                .WillCascadeOnDelete(true);
+        }
+    }
+}
