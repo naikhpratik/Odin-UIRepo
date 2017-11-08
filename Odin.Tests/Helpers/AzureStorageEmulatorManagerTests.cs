94085f892fbaca4858c864ea8aa649f1bad5e22e
Austin Emser <austin.emser@gmail.com>
2017-11-08 14:46:46 -0500

    Add azure storage emulator tests

---
+++ Odin.Tests/Helpers/AzureStorageEmulatorManagerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Odin.Helpers;
+
+namespace Odin.Tests.Helpers
+{
+    [TestClass]
+    public class AzureStorageEmulatorManagerTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            StopStorageEmulatorIfRunning();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            StopStorageEmulatorIfRunning();
+        }
+
+        private void StopStorageEmulatorIfRunning()
+        {
+            if (AzureStorageEmulatorManager.IsProcessRunning())
+            {
+                AzureStorageEmulatorManager.StopStorageEmulator();
+            }
+        }
+
+        [TestMethod]
+        public void StartStorageEmulator_WhenRunning_ShouldStartEmulator()
+        {
+            if (AzureStorageEmulatorManager.IsProcessRunning())
+            {
+                AzureStorageEmulatorManager.StopStorageEmulator();
+                AzureStorageEmulatorManager.IsProcessRunning().Should().BeFalse();
+            }
+
+            AzureStorageEmulatorManager.StartStorageEmulator();
+            AzureStorageEmulatorManager.IsProcessRunning().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void StopStorageEmulator_WhenRunning_EmulatorShouldBeStopped()
+        {
+            if (!AzureStorageEmulatorManager.IsProcessRunning())
+            {
+                AzureStorageEmulatorManager.StartStorageEmulator();
+                AzureStorageEmulatorManager.IsProcessRunning().Should().BeTrue();
+            }
+
+            AzureStorageEmulatorManager.StopStorageEmulator();
+            AzureStorageEmulatorManager.IsProcessRunning().Should().BeFalse();
+        }
+
+    }
+}
