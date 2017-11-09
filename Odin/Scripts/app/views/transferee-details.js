568c6c7078670e0a644780a78fab70505a5e03b6
Matt Akl <makl@DWELLWORKS.com>
2017-11-09 15:07:21 -0500

    Fixed the differences in javascript and the details page

--- Odin/Scripts/app/views/transferee-details.js
+++ Odin/Scripts/app/views/transferee-details.js
@@ -1,49 +1,52 @@
-﻿var TransfereeDetailsService = function (){
-     var route = "/api/orders/transferee/";
+﻿var TransfereeDetailsService = function () {
+    var route = "/api/orders/transferee/";
 
-var updateDetailsBlock = function (block, data, success, fail) {
-    var url = route + "/details/" + block;
-    $.post(url, data).done(success).fail(fail);
-     }
-return {
-    updateDetailsBlock: updateDetailsBlock    
-}
+    var updateDetailsBlock = function (block, data, success, fail) {
+        var url = route + "/details/" + block;
+        $.post(url, data).done(success).fail(fail);
+    }
+    return {
+        updateDetailsBlock: updateDetailsBlock
+    }
 
 }();
 var TransfereeDetailsController = function (transfereeDetailseService) {
-    
+
     var init = function () {
-       
-        $('.date').datetimepicker({    
+
+        var pnlDetails = $("div#details");
+        var detailsBlocks = pnlDetails.find("#servicesBlock");
+
+        detailsBlocks.find('.date').datetimepicker({
             format: "MM/DD/YY",
-            useCurrent:true,
+            useCurrent: true,
             keepOpen: false
         });
-        $('.time').datetimepicker({
+        detailsBlocks.find('.time').datetimepicker({
             format: 'LT',
             showClose: true,
             toolbarPlacement: 'bottom',
-            icons: { close: 'custom-icon-check'}
+            icons: { close: 'custom-icon-check' }
         });
-        pnlDetails = $("div#details");
 
-        pnlDetails.find(".details-header").find("span").on("click",
+
+        pnlDetails.find(".details-services").find("span").on("click",
             function () {
-                var cols = $(this).parents(".details-blocks").find(".details-row > .details-col");                                                                                           
-                cols.find("span").css("display", "block");                  
+                var cols = $(this).parents("[data-entity-collection = 'services']").find(".service-col");
+                cols.find("span").css("display", "block");
             });
 
         //Init Variables
-        detailsBlocks = pnlDetails.find(".details-blocks");
+        detailsBlocks = pnlDetails.find(".details-services");
         orderId = pnlDetails.attr("data-order-id");
 
         //Save Event for Services
-        detailsBlocks.on("click", ".details-save", saveBlock);
+        detailsBlocks.on("click", ".sectionSave", saveBlock);
     };
 
     var saveBlock = function (e) {
-
-        var detailsBlock = $(e.target).parents(".details-blocks");
+        ;
+        var detailsBlock = $(e.target).parents(".details-services");
         var block = detailsBlock.attr("data-block");
         var rows = detailsBlock.find(".details-row[data-entity-id]");
         var err = false;
@@ -55,7 +58,7 @@ var TransfereeDetailsController = function (transfereeDetailseService) {
         }
 
         var data = { "Id": orderId };
-       
+
         rows.each(function () {
             if (err)
                 return;
@@ -65,7 +68,7 @@ var TransfereeDetailsController = function (transfereeDetailseService) {
                 if (!isCollectionRowEmpty(rowInputs)) {
                     var collectionKey = row.attr("data-entity-collection");
                     var collectionData = { "Id": row.attr("data-entity-id") };
-                    
+
                     var ret = fillPostData(collectionData, rowInputs);
                     if (ret == -1) {
                         err = true;
@@ -79,10 +82,10 @@ var TransfereeDetailsController = function (transfereeDetailseService) {
             } else {
                 fillPostData(data, rowInputs);
             }
-            
+
         });
-         if (!err)            
-        TransfereeDetailsService.updateDetailsBlock(block, data, saveSuccess, saveFail);
+        if (!err)
+            TransfereeDetailsService.updateDetailsBlock(block, data, saveSuccess, saveFail);
     }
 
     var fillPostData = function (data, inputs) {
@@ -95,16 +98,16 @@ var TransfereeDetailsController = function (transfereeDetailseService) {
             st = input.attr("name") == "ScheduledTime" ? input.val() : st;
             cd = input.attr("name") == "CompletedDate" ? input.val() : cd;
         });
-        if (sd.length > 0 && st == '') {            
+        if (sd.length > 0 && st == '') {
             $('.text-danger[data-entity-id="' + data['Id'] + '"]').show();
-            return -1;            
+            return -1;
         }
-        else           
+        else
             $('.text-danger[data-entity-id="' + data['Id'] + '"]').hide();
 
-        data["ScheduledDate"] = sd + ' ' + st;        
+        data["ScheduledDate"] = sd + ' ' + st;
         data["CompletedDate"] = cd;
-        
+
     }
 
     var isCollectionRowEmpty = function (rowInputs) {
@@ -116,8 +119,7 @@ var TransfereeDetailsController = function (transfereeDetailseService) {
         return typeof attr !== typeof undefined && attr !== false && attr !== "" && attr !== null;
     }
 
-    var contains = function(value, searchFor)
-    {
+    var contains = function (value, searchFor) {
         return (value || '').indexOf(searchFor) > -1;
     }
 
@@ -125,17 +127,17 @@ var TransfereeDetailsController = function (transfereeDetailseService) {
         $.notify({
             message: message
         }, {
-            delay: 2000,
-            type: type,
-            placement: {
-                from: "bottom",
-                align: "center"
-            },
-            animate: {
-                enter: 'animated fadeInUp',
-                exit: 'animated fadeOutDown'
-            }
-        });
+                delay: 2000,
+                type: type,
+                placement: {
+                    from: "bottom",
+                    align: "center"
+                },
+                animate: {
+                    enter: 'animated fadeInUp',
+                    exit: 'animated fadeOutDown'
+                }
+            });
     }
 
 
