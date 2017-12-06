512683accf127e15d6ee435c22389340d16cbf6d
Austin Emser <austin.emser@dwellworks.com>
2017-12-06 11:47:24 -0500

    Add endpoint helper tests, create service processor

---
+++ Odin.ToSeWebJob.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Odin.ToSeWebJob.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Odin.ToSeWebJob.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58abab58-8bd4-4bc8-b44f-9bb397316a0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Odin.ToSeWebJob/ServiceProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Odin.Data.Core;
+
+namespace Odin.ToSeWebJob
+{
+    public class ServiceProcessor : IServiceProcessor
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ServiceProcessor(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void ProcessService(string serviceId)
+        {
+            var service = _unitOfWork.Services.GetServiceById(serviceId);
+
+        }
+    }
+
+    public interface IServiceProcessor
+    {
+        void ProcessService(string serviceId);
+    }
+}
