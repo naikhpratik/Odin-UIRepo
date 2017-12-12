9102e953a28de0ae46e112318bde19a1309308f9
Chris Nurre <CNurre@dwellworks.com>
2017-12-12 13:00:27 -0500

    Fixing the add appointment datepicker format

--- Odin/Scripts/app/views/transferee-itin-appntmt.js
+++ Odin/Scripts/app/views/transferee-itin-appntmt.js
@@ -23,7 +23,7 @@ var TransfereeAppointmentController = function (transfereeItineraryAppointment) 
     var init = function () { 
         var appointmentModal = $("div#appointmentModal");        
         appointmentModal.find('.date').datetimepicker({    
-            format: "DD-MMM-YYYY HH:MM TT",
+            format: "DD-MMM-YYYY HH:MM A",
             showClose: true,
             toolbarPlacement: 'bottom',
             icons: { close: 'custom-icon-check' },
