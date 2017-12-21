d4f8e3a1e16848552f18bf744528a113e14fe60b
Austin Emser <austin.emser@dwellworks.com>
2017-12-21 13:17:44 -0500

    Force HTTPS

--- Odin/App_Start/FilterConfig.cs
+++ Odin/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Odin
         {
             //filters.Add(new HandleErrorAttribute());
             filters.Add(new AiHandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
