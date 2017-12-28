6065363f2ca6bf8b038a1c93b199de65df7f6c2e
Austin Emser <austin.emser@dwellworks.com>
2017-12-28 11:05:16 -0500

    Add transferee invite status to transfere object

---
+++ Odin.Data/Core/Models/InviteStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin.Data.Core.Models
+{
+    public static class InviteStatus
+    {
+        public static string NotYetInvited = "NotYetInvited";
+        public static string Invited = "Invited";
+        public static string Accepted = "Accepted";
+    }
+}
