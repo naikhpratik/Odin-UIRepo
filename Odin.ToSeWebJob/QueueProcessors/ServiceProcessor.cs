73fdbfeffaf5bb57fa97f30b2efa435e31ea0ada
Austin Emser <austin.emser@dwellworks.com>
2018-01-24 15:21:36 -0500

    Add textwriter to process service to add additional logging

--- Odin.ToSeWebJob/QueueProcessors/ServiceProcessor.cs
+++ Odin.ToSeWebJob/QueueProcessors/ServiceProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -20,14 +21,16 @@ namespace Odin.ToSeWebJob.QueueProcessors
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<string> ProcessService(string serviceId)
+        public async Task<string> ProcessService(string serviceId, TextWriter log)
         {
             var service = _unitOfWork.Services.GetServiceById(serviceId);
             var endpoint = EndPointHelper.GetEndPointForService(service);
 
             var url = ConfigHelper.GetBaldrApiUrl() + endpoint;
-     
+            log.WriteLine($"url: {url}");
+
             var jsonData = ServiceToJson.GetJsonDataForService(service);
+            log.WriteLine($"jsonData {jsonData}");
 
             var request = RequestHelper.CreateBaldrRequest(ConfigHelper.GetSeApiToken(), url, jsonData);
 
