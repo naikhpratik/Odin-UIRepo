05972607d8c414872420d991a52bdeb2906356f4
Poloha <spoloha@dwellworks.com>
2018-02-09 12:42:59 -0500

    Refactoring and testing in prep for master merge.

--- Odin/App_Start/BundleConfig.cs
+++ Odin/App_Start/BundleConfig.cs
@@ -64,27 +64,17 @@ namespace Odin
 
 
             bundles.Add(new StyleBundle("~/Content/cssBundle").Include(
-                      "~/Content/styles/css/bootstrap-paper.css",
-                      "~/Content/styles/css/bootstrap-datetimepicker.css",
-                      "~/Content/styles/css/animate.css",
                       "~/Content/styles/css/application.css"));
-
 
             // Page Styling
             bundles.Add(new StyleBundle("~/Styling/help").Include("~/Content/styles/css/refactor/help.css"));
-            bundles.Add(new StyleBundle("~/Styling/orders").Include("~/Content/styles/css/refactor/orders.css", "~/Content/styles/css/typeahead.css"));
+            bundles.Add(new StyleBundle("~/Styling/orders").Include("~/Content/styles/css/refactor/orders.css"));
             bundles.Add(new StyleBundle("~/Styling/login").Include("~/Content/styles/css/refactor/login.css"));
             bundles.Add(new StyleBundle("~/Styling/forgotPassword").Include("~/Content/styles/css/refactor/forgotpassword.css"));
             bundles.Add(new StyleBundle("~/Styling/bookmarklet").Include("~/Content/styles/css/refactor/bookmarklet.css"));
             bundles.Add(new StyleBundle("~/Styling/transferee").Include("~/Content/styles/css/refactor/transferee.css"));
             bundles.Add(new StyleBundle("~/Styling/itineraryPdf").Include("~/Content/styles/css/refactor/itineraryPdf.css"));
             bundles.Add(new StyleBundle("~/Styling/propertiesPDF").Include("~/Content/styles/css/refactor/transferee/HFPropertiesPDF.css"));
-            bundles.Add(new StyleBundle("~/Styling/selectedProperty").Include("~/Content/styles/css/refactor/selectedProperty.css"));
-
-
-
-
-
 
         }
     }
